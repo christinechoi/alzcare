a5aacd9118d25a599c8beb5a77ecbb7659ab3b9f
christinechoi <smi77y@gmail.com>
2018-05-29 19:57:53 -0400

    Add proxy server and edit action to return json

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,13 +1,21 @@
 import fetch from 'isomorphic-fetch';
 
 export function getPhotos(input) {
+  const proxyURL = 'https://floating-cove-74260.herokuapp.com/'
+  const url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' + input + '&format=json&nojsoncallback=1'
+  debugger;
   return (dispatch) => {
-    return fetch('https://api.flickr.com/services/feeds/photos_public.gne' + input)
+    return fetch(proxyURL + url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(input)
+      })
       .then(response => {
         if (response.status >= 400) {
-          throw new Error("Sorry, there was a bad response from the server.");
+          throw new Error('Sorry, but there was an error from the server.');
         } 
-        console.log(response);
         return response.json();
       })
       .then(responseJson =>  {
@@ -15,3 +23,4 @@ export function getPhotos(input) {
       })
   }
 }
+

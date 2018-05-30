80c2a5f17c591af514efc53813d8ddab5c4281d7
christinechoi <smi77y@gmail.com>
2018-05-30 12:13:23 -0400

    Create Cards for photos

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+
 
 const Photo = (props) => {
   return (
     <div>
-      Photo component here
-      <img src="https://www.w3schools.com/images/picture.jpg" />
+      <Card.Group className='ui five column doubling stackable grid container centered'> {
+        props.photos.map((photo, index) => (  
+          <Card key={index} >
+            <Image src={photo.media.m} />
+            <Card.Content >
+              <Card.Header> {photo.title} </Card.Header> 
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
     </div>
   );
 };

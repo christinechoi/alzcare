d692200bcbb306eac69e2a5cd0b4eb60665507b7
christinechoi <smi77y@gmail.com>
2018-05-30 12:10:15 -0400

    Clean up code

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,6 @@ import fetch from 'isomorphic-fetch';
 export function getPhotos(input) {
   const proxyURL = 'https://floating-cove-74260.herokuapp.com/'
   const url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' + input + '&format=json&nojsoncallback=1'
-  debugger;
   return (dispatch) => {
     return fetch(proxyURL + url, {
       method: 'POST',
